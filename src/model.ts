2b3e4ae4a47fdac00c3712acd6bb23c5fec8610a
jswwWorks <jsww.works@gmail.com>
2024-01-29 17:11:56 -0500

    Updated populateShows function to display images from searched shows

--- src/model.ts
+++ src/model.ts
@@ -35,25 +35,6 @@ async function searchShowsByTerm(term: string): Promise<Array<IShow>> {
   )
   return filteredShows;
 }
-// return [
-//   {
-//     id: 1767,
-//     name: "The Bletchley Circle",
-//     summary:
-//       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-//          women with extraordinary skills that helped to end World War II.</p>
-//        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-//          normal lives, modestly setting aside the part they played in
-//          producing crucial intelligence, which helped the Allies to victory
-//          and shortened the war. When Susan discovers a hidden code behind an
-//          unsolved murder she is met by skepticism from the police. She
-//          quickly realises she can only begin to crack the murders and bring
-//          the culprit to justice with her former friends.</p>`,
-//     image:
-//       "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-//   }
-// ]
-
 
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
